6fd9389f14ab1980b9cf56c8feb2f5ece546f8ac
suneel <suneel@ceoitbox.in>
2024-06-19 13:53:05 -0400

    install typeorm

--- test/user/register.spec.ts
+++ test/user/register.spec.ts
@@ -1,6 +1,21 @@
 import request from "supertest";
 import app from "../../src/app";
+import { DataSource } from "typeorm";
+import { AppDataSource } from "../../src/config/data-source";
+import { truncateTables } from "../utils";
+import { User } from "../../src/entity/User";
 describe("POST /auth/register", () => {
+    let connection: DataSource;
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+    beforeEach(async () => {
+        // Database truncate
+        await truncateTables(connection);
+    });
+    afterAll(async () => {
+        await connection.destroy();
+    });
     describe("Given all fields", () => {
         it("should return 201 status could", async () => {
             // AAA
@@ -45,14 +60,12 @@ describe("POST /auth/register", () => {
             password: "password",
         };
         // Act
-        const response = await request(app)
-            .post("/auth/register")
-            .send(userData);
+        await request(app).post("/auth/register").send(userData);
         // Assert
 
-        expect(response.headers["content-type"]).toEqual(
-            expect.stringContaining("json"),
-        );
+        const userRepo = connection.getRepository(User);
+        const users = await userRepo.find();
+        expect(users).toHaveLength(1);
     });
     describe("Fields are missing", () => {});
 });
